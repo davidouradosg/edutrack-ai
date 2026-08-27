f9aab16a0313c2db0683cd0c32a94fe17f822dca
Davi Dourado de Siuza Goncalves <davi.goncalves@aluno.impacta.edu.br>
2026-08-27 00:45:30 -0300

    adiciona 15 exercicios de equacao

---
+++ 15tarefas/ex1.py
@@ -0,0 +1,8 @@
+# SOMA DE DOIS NUMEROS
+print("="*30)
+print("SOMA")
+x1= int(input("Digite um numero:"))
+x2= int(input("Digite um numero:"))
+resultado= x1+x2
+print(f"{x1} + {x2} = {resultado}")
+print("="*30)
---
+++ 15tarefas/ex2.py
@@ -0,0 +1,9 @@
+# MEDIA DE DUAS NOTAS
+
+x1= float(input("Digite um numero"))
+x2= float(input("Digite um numero"))
+resultado= (x1+x2)/2
+print(f"""NOTA1: {x1} 
+NOTA2: {x2}
+MÉDIA: {resultado}""")
+print("="*30)
